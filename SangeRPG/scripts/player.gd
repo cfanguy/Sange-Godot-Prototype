c4b5ec5fd6d14e6a00afe44ddf1072daf704b717
Chad M Fanguy <cfanguy@gmail.com>
2023-11-26 22:57:14 -0500

    Added a simple combat system
    Collision causes enemy to just disappear for now.

--- SangeRPG/scripts/player.gd
+++ SangeRPG/scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const speed = 400
 var direction = "down"
+var is_attacking = false
+var enemy = null
 
 @onready var anim = $AnimatedSprite2D
 
@@ -15,6 +17,26 @@ func handleInput():
 	var moveDirection = Input.get_vector("move_left", "move_right",
 	"move_up", "move_down",)
 	velocity = moveDirection * speed
+	if Input.is_action_pressed("attack"):
+		is_attacking = true
+		$AnimatedSprite2D.visible = false
+		
+		if enemy is CharacterBody2D:
+			enemy.visible = false
+		
+		if direction == "down":
+			$DownAction.visible = true
+			$AnimationPlayer.play("down_swing")
+		elif direction == "up":
+			$UpAction.visible = true
+			$AnimationPlayer.play("up_swing")
+		elif direction == "side":
+			if(anim.flip_h):
+				$RightAction.visible = true
+				$AnimationPlayer.play("right_swing")
+			else:
+				$LeftAction.visible = true
+				$AnimationPlayer.play("left_swing")
 
 func updateAnim():
 	if velocity.length() == 0:
@@ -33,4 +55,19 @@ func updateAnim():
 			direction = "up"
 
 		anim.play(direction + "_walk")
-	
+
+func _on_animation_player_animation_finished(anim_name):
+	is_attacking = false
+	$AnimationPlayer.stop()
+	$DownAction.visible = false
+	$UpAction.visible = false
+	$LeftAction.visible = false
+	$RightAction.visible = false
+	$AnimatedSprite2D.visible = true
+
+func _on_hit_box_body_entered(body):
+	enemy = body
+
+
+func _on_hit_box_body_exited(body):
+	enemy = null
