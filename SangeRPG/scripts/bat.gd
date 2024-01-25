5d6a86a53b9c3dbfc3337d68468a33e7e12a3e84
Chad M Fanguy <cfanguy@gmail.com>
2024-01-25 00:42:27 -0500

    Added attacking and health

--- SangeRPG/scripts/bat.gd
+++ SangeRPG/scripts/bat.gd
@@ -9,6 +9,13 @@ var direction_change_interval = 3
 var attack = false
 var player = null
 var player_in_range = false
+var health = 50
+var is_dead = false
+var damage_timer = 0.0
+var damage_interval = 1
+var damage_from_player_timer = 0.0
+var damage_from_player_interval = 1
+var is_attacking = false
 
 var min_position = Vector2(0, 0)
 var max_position = Vector2(4000, 3000)
@@ -19,6 +26,16 @@ func _ready():
 	add_to_group("Enemy")
 	
 func _physics_process(delta):
+	if player_in_range:
+		damage_from_player_timer += delta
+	if damage_from_player_timer >= damage_from_player_interval and player.is_attacking:
+		health -= 25
+		print("Bat Health: ", health)
+		damage_from_player_timer = 0.0
+	
+	update_health()
+	die()
+	
 	if attack:
 		var direction_to_player = (player.position - position).normalized()
 		velocity = direction_to_player * attack_speed
@@ -40,14 +57,26 @@ func _physics_process(delta):
 	
 	if old_position != position:
 		last_direction = -last_direction
+	
+	if player_in_range:
+		damage_timer += delta
+	if damage_timer >= damage_interval:
+		player.health -= 10
+		damage_timer = 0.0
 
 func update_animation(direction, attacking=false):
+	if is_dead:
+		return
+	
 	if player_in_range and attacking:
 		animated_sprite.play("attack")
 		animated_sprite.flip_h = direction.x < 0
+		is_attacking = true
 		return
 	else:
 		animated_sprite.play("idle")
+	
+	is_attacking = false
 	
 	animated_sprite.flip_h = false
 	
@@ -61,18 +90,33 @@ func pick_random_direction():
 	new_direction = new_direction.normalized()
 	last_direction = new_direction
 
+func update_health():
+	var healthbar = $healthbar
+	healthbar.value = health
+	if health >= 50:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+		
+func die():
+	if health <= 0 and not is_dead:
+		is_dead = true
+		queue_free()
+	
 
 func _on_bat_hitbox_body_entered(body):
 	if body.is_in_group("Player"):
 		player_in_range = true
 		print("Attacking!")
 		animated_sprite.flip_h = position.x > body.position.x
+		attack_speed = 0
 
 func _on_bat_hitbox_body_exited(body):
 	if body.is_in_group("Player"):
 		player_in_range = false
 		print("Player exited hitbox!")
 		animated_sprite.flip_h = false
+		attack_speed = 300
 
 func _on_territory_body_entered(body):
 	if body.is_in_group("Player"):
--- SangeRPG/scripts/player.gd
+++ SangeRPG/scripts/player.gd
@@ -4,12 +4,41 @@ const speed = 500
 var last_direction = Vector2.ZERO
 var animated_sprite
 var enemy_in_range = false
+var health = 100
+var is_dead = false
+var is_attacking = false
+var attack_timer = 0.0
+var attack_duraction = 1.0
 
 func _ready():
 	animated_sprite = $AnimatedSprite2D
 	add_to_group("Player")
 
 func _physics_process(delta):
+	update_health()
+	die()
+	update_animation()
+	move_and_slide()
+	
+	if is_attacking:
+		attack_timer += delta
+	if attack_timer >= attack_duraction:
+		is_attacking = false
+		attack_timer = 0.0
+	
+func update_animation():
+	if is_dead:
+		return
+	
+	if is_attacking:
+		if last_direction.y < 0:
+			animated_sprite.play("attack_up")
+		elif last_direction.y > 0:
+			animated_sprite.play("attack_down")
+		elif last_direction.x != 0:
+			animated_sprite.play("attack_left")
+		return
+	
 	var direction = Input.get_vector("ui_left", "ui_right",
 		"ui_up", "ui_down").normalized()
 	velocity = direction * speed
@@ -33,7 +62,23 @@ func _physics_process(delta):
 	
 	animated_sprite.flip_h = last_direction.x > 0
 
-	move_and_slide()
+func _input(event):
+	if event.is_action_pressed("ui_select"):
+		is_attacking = true
+		attack_timer = 0.0
+
+func update_health():
+	var healthbar = $healthbar
+	healthbar.value = health
+	if health >= 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+		
+func die():
+	if health <= 0 and not is_dead:
+		is_dead = true
+		animated_sprite.play("die")
 
 
 func _on_hitbox_body_entered(body):
@@ -46,3 +91,8 @@ func _on_hitbox_body_exited(body):
 	if body.is_in_group("Enemy"):
 		enemy_in_range = false
 		print("Enemy left hibox!")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "die":
+		queue_free()
